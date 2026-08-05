ee78ad8ba7fcdb6e0ff7b2c005d848d051d8df62
AlvaroSagitario <perceptionmaker@gmail.com>
2026-08-05 15:56:03 -0500

    Update codiguito.py
    AGREGADA LA CONVERSIÓN A YENES
    
    ÚLTIMA MODIFICACIÓN POR ALVARO YPORRE

--- codiguito.py
+++ codiguito.py
@@ -1,5 +1,5 @@
 # ===========================================
-# CONVERSOR DE MONEDAS - VERSIÓN 3
+# CONVERSOR DE MONEDAS - VERSIÓN 4
 # Autor(a): ALVARO YPORRE ALEJOS
 # ===========================================
 
@@ -8,6 +8,7 @@ soles = float(input("Ingrese la cantidad en soles: "))
 print("1. Dólares")
 print("2. euros")
 print("3. libras esterlinas")
+print("4. yenes")
 
 opcion = input("Selecione una opción: ")
 
@@ -19,9 +20,13 @@ elif opcion == "2":
     euros = soles/4.20
     print("Equivale a ", euros, "euros.")
 
-elif opcion=="3":
+elif opcion =="3":
     libras = soles/4.80
     print("Equivale a ", libras, "libras.")
+
+elif opcion == "4":
+    yenes = soles * 41.5
+    print("Equivale a ", yenes, "yenes")
 
 else:
     print("Opción no válida")
